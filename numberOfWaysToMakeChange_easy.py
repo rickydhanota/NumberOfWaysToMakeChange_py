8b570869961a293bcc7280c343445ed1b80aca91
Ricky <rickdhanota@gmail.com>
2020-10-25 17:58:17 -0700

    initial commit

---
+++ numberOfWaysToMakeChange_easy.py
@@ -0,0 +1,12 @@
+# Number of ways to make change
+#Given an array of distinct positive integers representing coin denominations and a single non negative integer n representing a target amount of money. Write a function that returns the number of ways to make change for that target amount using the given coin denominations.
+# Note that an unlimited amount of coins is at your disposal
+
+# n = 6
+# denom = [1, 5]
+
+#Sample output
+# 2 // 1x1 + 1x5 and 6x1
+
+def numberOfWaysToMakeChange(n, denom):
+    pass
