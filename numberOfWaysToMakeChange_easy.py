4cdec9d140fd1d0b3c3f9409d890531113e98c31
Ricky <rickdhanota@gmail.com>
2020-10-25 18:10:02 -0700

    trying to solve the palgo

--- numberOfWaysToMakeChange_easy.py
+++ numberOfWaysToMakeChange_easy.py
@@ -8,5 +8,13 @@
 #Sample output
 # 2 // 1x1 + 1x5 and 6x1
 
-def numberOfWaysToMakeChange(n, denom):
-    pass
+def numberOfWaysToMakeChange(n, denoms):
+    ways = [0 for amount in range(n + 1)]
+    ways[0] = 1
+    for denom in denoms:
+        for amount in range(1, n + 1):
+            if denom <= amount:
+                ways[amount] += ways[amount - denom]
+    return ways[n]
+
+print(numberOfWaysToMakeChange(6, [1, 5]))
